b0b5d7a8c4291d24e59b456d3a7295972a30473c
Teo <teosany@icloud.com>
2024-12-10 19:35:42 +0100

    fix: refactor test_custom_sort_service to handle multiple data types

--- analytics/tests/test_custom_sort_service.py
+++ analytics/tests/test_custom_sort_service.py
@@ -15,10 +15,20 @@ class CustomSortServiceTests(TestCase):
     def test_custom_sort(self):
         """Test sorting with real data from CSV file"""
         with open('analytics/static/data/data_types.csv', 'r', encoding='utf-8-sig') as file:
-            next(file)
-            real_data = set(line.split(';')[2].strip() for line in file)
+            next(file);next(file)
 
-        result = CustomSortService.custom_sort(list(real_data))
+            typologies = set()
+            for line in file:
+                if line.strip():
+                    column = line.split(';')[4]
+                    types = [t.strip() for t in column.split('"),') if t.strip()]
+                    types = [t + '")' if t.count('(') > t.count(')') else t for t in types]
+                    typologies.update(types)
+
+        result = CustomSortService.custom_sort(list(typologies))
+
+        for item in result:
+            print(f"Len {len(item)}: {item}")
 
         for i in range(len(result) - 1):
             if len(result[i]) == len(result[i + 1]):
