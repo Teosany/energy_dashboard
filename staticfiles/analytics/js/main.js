082a59e35fedeacaf1330805f5a0a383195c3bc3
Teo <teosany@icloud.com>
2024-12-10 14:29:49 +0100

    fix: static files handling and build process

--- staticfiles/analytics/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initCharts } from './charts.js';
-import { initFileUpload } from './file-upload.js';
-import { initApiModal } from './api-modal.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initCharts();
-    initFileUpload();
-    initApiModal();
-});
